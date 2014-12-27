f88116387098cf2cc91b11bd532192dca990b252
panhzh3 <haizhipan@gmail.com>
2014-12-27 18:05:21 +0800

    ....

--- items/views.py
+++ items/views.py
@@ -1,1 +1,31 @@
+#coding:utf-8
 # Create your views here.
+from datetime import datetime
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from models import *
+from forms import *
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.models import User
+from PIL import ImageFile
+
+@login_required
+def fitting(request):
+    user = request.user
+    try:
+        profile = user.buyerprofile
+    except:
+        pass
+    try:
+        profile = user.sellerprofile
+    except:
+        pass
+    if not profile:
+        return HttpResponse('You are admin.')
+    return render_to_response('fitting.html', RequestContext(request, {'profile': profile}),)
+
